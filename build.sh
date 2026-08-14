51ffe0e2a0482a6ca9197d178e3526fa9c93eda6
marcusblai32-ctrl <marcusblai32@gmail.com>
2026-08-14 11:42:31 -0400

    Update build script for dependency installation and migration

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. Enstale depandans yo
 pip install -r requirements.txt
+
+# 2. Kolekte fich statik yo
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# 3. Fè migrasyon baz done a
+python manage.py migrate --no-input
+
+# 4. Kreye superuser (si li pa egziste)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@educdim.com').exists() or User.objects.create_superuser('admin@educdim.com', 'Admin', 'EducDim', 2000, 'admin12345')" | python manage.py shell
