91d52d62c185c1d7c99b950da7b0449d223c7572
marcusblai32-ctrl <marcusblai32@gmail.com>
2026-08-20 13:01:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "=== Installation ==="
 pip install -r requirements.txt
-
-echo "=== Compile messages (sèlman ht ak fr) ==="
-python manage.py compilemessages --locale ht --locale fr
-
-echo "=== Collect static ==="
 python manage.py collectstatic --noinput
-
-echo "=== Migrations ==="
 python manage.py migrate --noinput
-
-echo "=== Build terminé ==="
