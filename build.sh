fac0e06600fe32ba974f9867933ad961928dae58
marcusblai32-ctrl <marcusblai32@gmail.com>
2026-08-20 12:51:31 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 echo "=== Installation ==="
 pip install -r requirements.txt
 
-echo "=== Compile messages (tradiksyon) ==="
-python manage.py compilemessages
+echo "=== Compile messages (sèlman ht ak fr) ==="
+python manage.py compilemessages --locale ht --locale fr
 
 echo "=== Collect static ==="
 python manage.py collectstatic --noinput
