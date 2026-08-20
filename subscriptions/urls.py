c226e07fe8c31a26d241222d52b357aa9f9b2329
marcusblai32-ctrl <marcusblai32@gmail.com>
2026-08-20 10:03:39 -0500

    Update urls.py

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,9 +4,14 @@ from . import views
 app_name = 'subscriptions'
 
 urlpatterns = [
+    # URLs ki deja egziste
     path('', views.subscription_list, name='list'),
-    path('abonner/<int:plan_pk>/', views.subscribe, name='subscribe'),
+    path('souscrire/<int:plan_pk>/', views.subscribe, name='subscribe'),
     path('mes-abonnements/', views.my_subscriptions, name='my_subscriptions'),
-    path('mon-abonnement/<int:pk>/', views.subscription_detail, name='detail'),
+    path('detail/<int:pk>/', views.subscription_detail, name='detail'),
     path('admin/en-attente/', views.admin_pending, name='admin_pending'),
+    path('admin/verifier/<int:pk>/', views.admin_verify_subscription, name='admin_verify'),
+    
+    # ===== NOUVO URLs =====
+    path('chwazi-kou/<int:pk>/', views.select_courses, name='select_courses'),
 ]
