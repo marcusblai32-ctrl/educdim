0d63455f55d6c892d33694dc4442358723e8c733
marcusblai32-ctrl <marcusblai32@gmail.com>
2026-08-20 23:42:28 -0400

    Update admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Quiz, Question, Reponse, TentativeQuiz, ReponseUtilisateur
 
 
@@ -14,7 +16,9 @@ class QuestionInline(admin.TabularInline):
     fieldsets = (
         (None, {'fields': ('type_question', 'texte', 'explication', 'points', 'ordre')}),
         ('Média de la question', {
-            'fields': ('q_media_titre', 'q_media_audio_url', 'q_media_audio_file', 'q_media_video_url', 'q_media_video_file', 'q_media_image_url', 'q_media_image_file', 'q_media_texte'),
+            'fields': ('q_media_titre', 'q_media_audio_url', 'q_media_audio_file', 
+                      'q_media_video_url', 'q_media_video_file', 'q_media_image_url', 
+                      'q_media_image_file', 'q_media_texte'),
             'classes': ('collapse',)
         }),
     )
@@ -35,7 +39,8 @@ class QuizAdmin(admin.ModelAdmin):
             'description': 'Chwazi youn nan twa: Cours, Module, oswa Leçon.'
         }),
         ('Média du Quiz', {
-            'fields': ('media_titre', 'media_audio_url', 'media_audio_file', 'media_video_url', 'media_video_file', 'media_image_url', 'media_image_file', 'media_texte'),
+            'fields': ('media_titre', 'media_audio_url', 'media_audio_file', 'media_video_url', 
+                      'media_video_file', 'media_image_url', 'media_image_file', 'media_texte'),
             'classes': ('collapse',)
         }),
     )
@@ -57,7 +62,9 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('quiz', 'type_question', 'texte', 'explication', 'points', 'ordre')}),
         ('Média de la question', {
-            'fields': ('q_media_titre', 'q_media_audio_url', 'q_media_audio_file', 'q_media_video_url', 'q_media_video_file', 'q_media_image_url', 'q_media_image_file', 'q_media_texte'),
+            'fields': ('q_media_titre', 'q_media_audio_url', 'q_media_audio_file', 
+                      'q_media_video_url', 'q_media_video_file', 'q_media_image_url', 
+                      'q_media_image_file', 'q_media_texte'),
             'classes': ('collapse',)
         }),
     )
@@ -65,10 +72,22 @@ class QuestionAdmin(admin.ModelAdmin):
 
 @admin.register(TentativeQuiz)
 class TentativeQuizAdmin(admin.ModelAdmin):
-    list_display = ('utilisateur', 'quiz', 'score', 'reussi', 'date_debut', 'date_soumission')
-    list_filter = ('reussi',)
+    list_display = ('utilisateur', 'quiz', 'score', 'reussi', 'date_debut', 'date_soumission', 'lien_correction')
+    list_filter = ('reussi', 'quiz')
     search_fields = ('utilisateur__email', 'quiz__titre')
     readonly_fields = ('date_debut', 'date_soumission')
+    
+    def lien_correction(self, obj):
+        """Ajoute yon bouton pou korije tentativ la"""
+        if obj.date_soumission:  # Sèlman si tentativ la soumise
+            url = reverse('quiz:corriger_tentative', args=[obj.id])
+            return format_html(
+                '<a class="button" href="{}" style="background:#28a745;color:white;padding:5px 10px;border-radius:4px;text-decoration:none;" target="_blank">✏️ Korije</a>',
+                url
+            )
+        return "-"
+    lien_correction.short_description = "Koreksyon"
+    lien_correction.allow_tags = True
 
 
 @admin.register(ReponseUtilisateur)
@@ -76,3 +95,5 @@ class ReponseUtilisateurAdmin(admin.ModelAdmin):
     list_display = ('tentative', 'question')
     search_fields = ('tentative__utilisateur__email', 'question__texte')
     readonly_fields = ('audio_reponse', 'video_reponse', 'image_reponse', 'fichier_reponse')
+    fields = ('tentative', 'question', 'reponses_selectionnees', 'texte_reponse',
+              'audio_reponse', 'video_reponse', 'image_reponse', 'fichier_reponse')
