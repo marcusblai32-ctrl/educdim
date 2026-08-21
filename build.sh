f816ae430520753b672ad5b8fafa9ffee63a40da
marcusblai32-ctrl <marcusblai32@gmail.com>
2026-08-20 23:38:42 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
 
-# Run database migrations
+# Fè migrasyon
 python manage.py makemigrations
 python manage.py migrate
+
+# Ajoute kolòn nan si li pa egziste
+python manage.py dbshell << EOF
+DO \$\$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+                   WHERE table_name='quiz_reponseutilisateur' 
+                   AND column_name='points_attribues') THEN
+        ALTER TABLE quiz_reponseutilisateur ADD COLUMN points_attribues decimal(5,2) NULL;
+    END IF;
+END \$\$;
+EOF
